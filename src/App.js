fe82cf249182c1669b630f727f7a022649d91ec0
lorenzodentale <lorenzo.dentale.02@gmail.com>
2024-01-22 16:26:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import './bottone.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Ciao Bellissimi</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+        //<button class="btn" type="button">
+        //   <strong>Prova</strong>
+        //   <div id="container-stars">
+        //     <div id="stars"></div>
+        //   </div>
+        // 
+        //   <div id="glow">
+        //     <div class="circle"></div>
+        //     <div class="circle"></div>
+        //   </div>
+        // </button>
